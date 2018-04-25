3a07770609c983ab2e43bec9627cb516b13aff71
Thiago Sev <sivthiago@hotmail.com>
2018-04-25 11:16:20 -0300

    Initial Commit

---
+++ Manga Storage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Manga Storage
+//
+//  Created by Thiago Augusto on 25/04/18.
+//  Copyright © 2018 Thiago Augusto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
